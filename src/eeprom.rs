0b4f7c9201404fe6a0dc8fd9e8d6a5507e4c7c15
Robert Jördens <rj@quartiq.de>
2019-11-11 12:04:50 +0100

    remove unused code, deny warnings

--- src/eeprom.rs
+++ src/eeprom.rs
@@ -8,16 +8,3 @@ pub fn read_eui48<'a>(i2c: &pac::I2C2) -> Result<[u8; 6], i2c::Error> {
     i2c::write_read(i2c, I2C_ADDR, &[0xFAu8], &mut buffer)?;
     Ok(buffer)
 }
-
-pub fn read(i2c: &pac::I2C2, addr: u8, mut buffer: &mut [u8]) -> Result<(), i2c::Error> {
-    i2c::write_read(i2c, I2C_ADDR, &[addr], &mut buffer)?;
-    Ok(())
-}
-
-pub fn write(i2c: &pac::I2C2, addr: u8, buffer: &[u8]) -> Result<(), i2c::Error> {
-    for (i, &byte) in buffer.iter().enumerate() {
-        i2c::write(i2c, I2C_ADDR, &[addr+i as u8, byte])?;
-    }
-
-    Ok(())
-}
