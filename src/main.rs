5c4dec0870eeb277a78d85809e84fa338537afd3
Robert Jördens <rj@quartiq.de>
2020-06-23 14:16:33 +0200

    dac clr/ldac: unwrap

--- src/main.rs
+++ src/main.rs
@@ -325,9 +325,9 @@ const APP: () = {
             spi
         };
 
-        let _dac_clr_n = gpioe.pe12.into_push_pull_output().set_high();
-        let _dac0_ldac_n = gpioe.pe11.into_push_pull_output().set_low();
-        let _dac1_ldac_n = gpioe.pe15.into_push_pull_output().set_low();
+        let _dac_clr_n = gpioe.pe12.into_push_pull_output().set_high().unwrap();
+        let _dac0_ldac_n = gpioe.pe11.into_push_pull_output().set_low().unwrap();
+        let _dac1_ldac_n = gpioe.pe15.into_push_pull_output().set_low().unwrap();
 
         let dac0_spi = {
             let spi_miso = gpioe
