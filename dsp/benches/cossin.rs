5cd93d3318c7524b76c8b1b30b2a1c9d85d31e2e
Robert Jördens <rj@quartiq.de>
2020-12-11 17:08:16 +0100

    fmt

--- dsp/benches/cossin.rs
+++ dsp/benches/cossin.rs
@@ -2,9 +2,7 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use dsp::trig::cossin;
 
 fn cossin_bench(c: &mut Criterion) {
-    c.bench_function("cossin(0)", |b| {
-        b.iter(|| cossin(black_box(0)))
-    });
+    c.bench_function("cossin(0)", |b| b.iter(|| cossin(black_box(0))));
 }
 
 criterion_group!(benches, cossin_bench);
