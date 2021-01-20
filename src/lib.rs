26677063eaac28431c52403b929ca2a9739da1b0
Ryan Summers <ryan.summers@vertigo-designs.com>
2021-01-20 13:43:34 +0100

    Adding support for multiple applications

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+#![no_std]
+
+pub mod server;
+
+#[macro_use]
+extern crate log;
+
+pub mod hardware;
+
+// The number of ticks in the ADC sampling timer. The timer runs at 100MHz, so the step size is
+// equal to 10ns per tick.
+// Currently, the sample rate is equal to: Fsample = 100/256 MHz = 390.625 KHz
+const ADC_SAMPLE_TICKS: u16 = 256;
+
+// The desired ADC sample processing buffer size.
+const SAMPLE_BUFFER_SIZE: usize = 8;
