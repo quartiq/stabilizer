2a84e3f299af7b6459b9b73cf51fd9f7aa987010
Robert Jördens <rj@quartiq.de>
2021-02-01 18:37:05 +0100

    dsp: remove unused code, let the compiler decide about inlining

--- dsp/src/accu.rs
+++ dsp/src/accu.rs
@@ -12,7 +12,6 @@ impl Accu {
 
 impl Iterator for Accu {
     type Item = i32;
-    #[inline]
     fn next(&mut self) -> Option<i32> {
         let s = self.state;
         self.state = self.state.wrapping_add(self.step);
