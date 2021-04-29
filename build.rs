780b3441a21dbdbc3782a52d400cb7d1d84344f9
Robert Jördens <rj@quartiq.de>
2021-04-29 17:33:20 +0200

    fmt

--- build.rs
+++ build.rs
@@ -1,9 +1,6 @@
 fn main() {
     println!("cargo:rerun-if-changed=memory.x");
 
-    cc::Build::new()
-        .file("src/startup.S")
-        .compile("startup");
+    cc::Build::new().file("src/startup.S").compile("startup");
     println!("cargo:rerun-if-changed=src/startup.S");
 }
-
