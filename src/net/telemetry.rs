dfa5ef2d7b9fc3025d266738938135d8d1c4fbe6
Robert Jördens <rj@quartiq.de>
2022-11-03 11:36:18 +0000

    bump minimq

--- src/net/telemetry.rs
+++ src/net/telemetry.rs
@@ -11,7 +11,6 @@
 ///! required immediately before transmission. This ensures that any slower computation required
 ///! for unit conversion can be off-loaded to lower priority tasks.
 use heapless::{String, Vec};
-use minimq::{QoS, Retain};
 use serde::Serialize;
 
 use super::NetworkReference;
@@ -138,13 +137,12 @@ impl<T: Serialize> TelemetryClient<T> {
         let telemetry: Vec<u8, 512> =
             serde_json_core::to_vec(telemetry).unwrap();
         self.mqtt
-            .client
+            .client()
             .publish(
-                &self.telemetry_topic,
-                &telemetry,
-                QoS::AtMostOnce,
-                Retain::NotRetained,
-                &[],
+                minimq::Publication::new(&telemetry)
+                    .topic(&self.telemetry_topic)
+                    .finish()
+                    .unwrap(),
             )
             .map_err(|e| log::error!("Telemetry publishing error: {:?}", e))
             .ok();
