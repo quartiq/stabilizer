9066211936f20dd5fb17be6247a92eb52a7a1099
SingularitySurfer <norman_krackow@gmx.de>
2022-11-10 14:54:42 +0100

    fix branch name

--- py/setup.py
+++ py/setup.py
@@ -1,17 +1,19 @@
 from setuptools import setup, find_packages
 
-setup(name="stabilizer",
-      packages=find_packages(),
-      # Keep versions in Cargo.toml and py/setup.py synchronized.
-      version="0.8.0",
-      description="Stabilizer Utilities",
-      author="QUARTIQ GmbH",
-      license="MIT",
-      python_requires=">=3.8",
-      install_requires=[
-            "numpy",
-            "scipy",
-            "matplotlib",
-            "gmqtt",
-            "miniconf-mqtt@git+https://github.com/quartiq/miniconf@develop#subdirectory=py/miniconf-mqtt"
-      ])
+setup(
+    name="stabilizer",
+    packages=find_packages(),
+    # Keep versions in Cargo.toml and py/setup.py synchronized.
+    version="0.8.0",
+    description="Stabilizer Utilities",
+    author="QUARTIQ GmbH",
+    license="MIT",
+    python_requires=">=3.8",
+    install_requires=[
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "gmqtt",
+        "miniconf-mqtt@git+https://github.com/quartiq/miniconf@main#subdirectory=py/miniconf-mqtt",
+    ],
+)
