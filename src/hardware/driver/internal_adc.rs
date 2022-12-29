7e83add150168f374802fbc82f9d2bdad9c31eb7
SingularitySurfer <norman_krackow@gmx.de>
2022-12-29 15:37:38 +0100

    line breaks

--- src/hardware/driver/internal_adc.rs
+++ src/hardware/driver/internal_adc.rs
@@ -85,7 +85,9 @@ impl InternalAdc {
                 self.output_current.1.read_normalized().unwrap()
             }
         };
-        let scale = V_REF * I_SCALE * self.variant[ch as usize].output_transadmittance(); // current sense scale
+        let scale = V_REF
+            * I_SCALE
+            * self.variant[ch as usize].output_transadmittance(); // current sense scale
         ratio * scale
     }
 }
