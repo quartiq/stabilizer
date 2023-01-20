0d191296d2c72fd27238ff4c1eda82d1b70d11e0
occheung <dc@m-labs.hk>
2023-01-20 12:39:43 +0800

    ad9959: add docs for converters

--- ad9959/src/lib.rs
+++ ad9959/src/lib.rs
@@ -566,10 +566,19 @@ pub fn validate_clocking(
     Err(Error::Frequency)
 }
 
+/// Convert and validate frequency into frequency tuning word.
+///
+/// Arguments:
+/// * `dds_frequency` - The DDS frequency to be converted and validated.
+/// * `system_clock_frequency` - The system clock frequency of the AD9959 core.
+///
+/// Returns:
+/// The corresponding frequency tuning word.
 pub fn frequency_to_ftw(
     dds_frequency: f32,
     system_clock_frequency: f32,
 ) -> Result<u32, Error> {
+    // Output frequency should not exceed the Nyquist's frequency.
     if !(0.0..=(system_clock_frequency / 2.0)).contains(&dds_frequency) {
         return Err(Error::Bounds);
     }
@@ -578,10 +587,24 @@ pub fn frequency_to_ftw(
     Ok(((dds_frequency / system_clock_frequency) * (1u64 << 32) as f32) as u32)
 }
 
+/// Convert phase into phase offset word.
+///
+/// Arguments:
+/// * `phase_turns` - The phase of a DDS channel.
+///
+/// Returns:
+/// The corresponding phase offset word.
 pub fn phase_to_pow(phase_turns: f32) -> Result<u16, Error> {
     Ok((phase_turns * (1 << 14) as f32) as u16 & 0x3FFFu16)
 }
 
+/// Convert amplitude into amplitude control register values.
+///
+/// Arguments:
+/// * `amplitude` - The amplitude of a DDS channel.
+///
+/// Returns:
+/// The corresponding value in the amplitude control register.
 pub fn amplitude_to_acr(amplitude: f32) -> Result<u32, Error> {
     if !(0.0..=1.0).contains(&amplitude) {
         return Err(Error::Bounds);
