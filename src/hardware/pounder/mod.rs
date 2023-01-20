934af894414683ccbe32754ed19376dd045089c0
occheung <dc@m-labs.hk>
2023-01-19 22:56:04 -0800

    Profile: comment -> docstring

--- src/hardware/pounder/mod.rs
+++ src/hardware/pounder/mod.rs
@@ -113,15 +113,15 @@ impl Default for DdsChannelConfig {
 
 /// Represents a fully defined DDS profile, with parameters expressed in machine units
 pub struct Profile {
-    // A 32-bits representation of DDS frequency in relation to the system clock frequency.
-    // This value corresponds to the AD9959 CFTW0 register, which specifies the frequency
-    // of DDS channels.
+    /// A 32-bits representation of DDS frequency in relation to the system clock frequency.
+    /// This value corresponds to the AD9959 CFTW0 register, which specifies the frequency
+    /// of DDS channels.
     pub frequency_tuning_word: u32,
-    // The DDS phase offset. It corresponds to the AD9959 CPOW0 register, which specifies
-    // the phase offset of DDS channels.
+    /// The DDS phase offset. It corresponds to the AD9959 CPOW0 register, which specifies
+    /// the phase offset of DDS channels.
     pub phase_offset: u16,
-    // Control amplitudes of DDS channels. It corresponds to the AD9959 ACR register, which
-    // controls the amplitude scaling factor of DDS channels.
+    /// Control amplitudes of DDS channels. It corresponds to the AD9959 ACR register, which
+    /// controls the amplitude scaling factor of DDS channels.
     pub amplitude_control: u32,
 }
 
