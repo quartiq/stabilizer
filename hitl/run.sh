6d5085481f114b05d1fee0c39d5923b1b9e5fdc0
Robert Jördens <rj@quartiq.de>
2024-07-10 12:47:19 +0200

    same for hitl

--- hitl/run.sh
+++ hitl/run.sh
@@ -18,7 +18,7 @@ python3 -m venv --system-site-packages vpy
 . vpy/bin/activate
 
 # Install Miniconf utilities for configuring stabilizer.
-python3 -m pip install py
+python3 -m pip install py/
 
 probe-rs download --chip STM32H743ZITx --log-file /dev/null --probe 0483:3754:004C003D3137510D33333639 target/thumbv7em-none-eabihf/release/dual-iir 
 probe-rs reset --chip STM32H743ZITx --log-file /dev/null --probe 0483:3754:004C003D3137510D33333639 --connect-under-reset
