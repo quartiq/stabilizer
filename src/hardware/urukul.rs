4cd6ad058486a2a4c104b76edd2d9539b5e976f8
Robert Jördens <rj@quartiq.de>
2024-11-24 14:54:07 +0000

    urukul: cfg/sta

--- src/hardware/urukul.rs
+++ src/hardware/urukul.rs
@@ -1,6 +1,10 @@
 use log;
 use stm32h7xx_hal::{self as hal, prelude::*};
 
+use arbitrary_int::{u2, u24, u3, u4, u7};
+use bitbybit::{bitenum, bitfield};
+use core::fmt::Debug;
+
 pub struct Urukul {
     pub spi: hal::spi::Spi<hal::stm32::SPI6, hal::spi::Enabled, u8>,
     pub cs: [hal::gpio::ErasedPin<hal::gpio::Output>; 3],
@@ -9,7 +13,9 @@ pub struct Urukul {
 }
 
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
-pub enum Error {}
+pub enum Error {
+    InvalidProtoRev,
+}
 
 #[repr(u8)]
 #[derive(Copy, Clone, Debug, PartialEq, PartialOrd)]
@@ -22,6 +28,45 @@ pub enum Select {
     Dds1 = 5,
     Dds2 = 6,
     Dds3 = 7,
+}
+
+#[bitfield(u24, default = 0x000000)]
+#[derive(Debug, PartialEq)]
+pub struct Cfg {
+    #[bits(0..=3, rw)]
+    pub rf_sw: u4,
+    #[bits(4..=7, rw)]
+    pub led: u4,
+    #[bits(8..=10, rw)]
+    pub profile: u3,
+    // 11: dummy
+    #[bit(12, rw)]
+    pub io_update: bool,
+    #[bits(13..=16, rw)]
+    pub mask_nu: u4,
+    #[bits([17, 20], rw)]
+    clk_sel: u2,
+    #[bit(18, rw)]
+    sync_sel: bool,
+    #[bit(19, rw)]
+    rst: bool,
+    #[bits(23..=24, rw)]
+    div: u2,
+}
+
+#[bitfield(u24)]
+#[derive(Debug, PartialEq)]
+pub struct Status {
+    #[bits(0..=3, r)]
+    pub rf_sw: u4,
+    #[bits(4..=7, r)]
+    pub smp_err: u4,
+    #[bits(8..=11, r)]
+    pub pll_lock: u4,
+    #[bits(12..=15, r)]
+    pub ifc_mode: u4,
+    #[bits(16..=22, r)]
+    pub proto_rev: u7,
 }
 
 impl Urukul {
@@ -51,12 +96,24 @@ impl Urukul {
     }
 
     pub fn init(&mut self) -> Result<(), Error> {
-        let mut cfg = [0; 3];
-        let sta = self.transfer(Select::Cfg, &mut cfg).unwrap();
-        log::info!("{sta:?}");
+        let cfg = Cfg::default();
+        let mut cfg = cfg.raw_value().to_be_bytes();
+        self.transfer(Select::Cfg, &mut cfg).unwrap();
+        let sta = Status::new_with_raw_value(u24::from_be_bytes(cfg));
+        if sta.proto_rev() != u7::new(0x8) {
+            return Err(Error::InvalidProtoRev);
+        }
+        log::info!(
+            "rf_sw={:4b} smp_err={:4b} pll_lock={:4b} ifc_mode={:4b}",
+            sta.rf_sw(),
+            sta.smp_err(),
+            sta.pll_lock(),
+            sta.ifc_mode()
+        );
         let mut att = [0; 4];
         let attr = self.transfer(Select::Att, &mut att).unwrap();
         log::info!("{attr:?}");
+        log::info!("Urukul CPLD initialization complete.");
         Ok(())
     }
 }
